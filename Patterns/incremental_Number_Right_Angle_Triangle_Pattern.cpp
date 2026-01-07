20fc50e97597ae8584ca770255dc861e405d9f19
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-01-07 17:54:48 +0530

    Create incremental_Number_Right_Angle_Triangle_Pattern.cpp

---
+++ Patterns/incremental_Number_Right_Angle_Triangle_Pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    
+    for(int i=0;i<n;i++){
+        int a =1;
+        for(int j=0;j<i+1;j++){
+            cout << a+j << " ";
+            
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+/* Enter the number: 4
+1 
+1 2 
+1 2 3 
+1 2 3 4  */
