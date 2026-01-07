3c3c0363e5378176c525e610ec02d0b5b1f5cd9b
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-01-07 18:43:35 +0530

    Create centered-Palindromic_Number_Pyramid.cpp

---
+++ Patterns/centered-Palindromic_Number_Pyramid.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+        for(int j=0;j<i+1;j++){
+            cout << j+1;
+        }
+        for(int j=i;j>0;j--){
+            cout << j;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+/* Enter the number: 4
+   1
+  121
+ 12321
+1234321  */
