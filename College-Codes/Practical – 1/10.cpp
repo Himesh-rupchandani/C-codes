5ea03943f81fc0e43c08e28cd44e395877d9d576
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-01-20 22:58:47 +0530

    Create 10.cpp

---
+++ College-Codes/Practical – 1/10.cpp
@@ -0,0 +1,24 @@
+// Write a program to find the factorial of numbers.
+
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if(n==0) return 1;
+    return n*fact(n-1);
+}
+
+int main(){
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    cout << "Factorial of " << num << " is: " << fact(num);
+    
+    return 0;
+}
+
+/*
+Enter a number: 6
+Factorial of 6 is: 720
+*/
