c2a5f775733c52d5b610367f92d04d1ddaa1a6d0
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-02-01 23:23:45 +0530

    Update 7.cpp

--- College-Codes/Practical – 1/7.cpp
+++ College-Codes/Practical – 1/7.cpp
@@ -7,7 +7,7 @@ int main(){
     int n,a=0;
     cout << "Enter the number: ";
     cin >> n;
-    if(n==0 || n==1) a = 0;
+    if(n==0 || n==1) a = 1;
     
     for(int i=2;i<n;i++){
         if(n%i==0){
