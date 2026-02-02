319e238db3d1bf45bf282f0621507bb53d94e447
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-02-03 01:07:57 +0530

    Update 9.cpp

--- College-Codes/Practical – 1/9.cpp
+++ College-Codes/Practical – 1/9.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main(){
-    int n,a,sum = 0;
+    int n,m,sum = 0;
     cout << "Enter the number: ";
     cin >> n;
     
     while(n>0){
-        a = n%10;
-        sum = sum + a;
+        m = n%10;
+        sum += m;
         n = n/10;
     }
     cout << "The sum of a digits is: " << sum << endl;
