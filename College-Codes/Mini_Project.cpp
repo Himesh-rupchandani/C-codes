5146693a4ab0651e8b5c34a2aa4e9b6944d1afe0
Himesh Rupchandani <rupchandanihimesh69@gmail.com>
2026-02-22 21:02:16 +0530

    Update Mini_Project.cpp

--- College-Codes/Mini_Project.cpp
+++ College-Codes/Mini_Project.cpp
@@ -20,7 +20,7 @@ void print1(){
 
 int main(){
      int totalItem;
-     double totalTax = 0,Price = 0,grandTotal = 0;
+     double totalTax = 0,grandTotal = 0;
      string name;
 
      cout << "Enter Name of Customer: ";
@@ -29,19 +29,22 @@ int main(){
      cout << "Enter an Number for Total Item: ";
      cin >> totalItem;;
      string names[100];
-     string item[100];
      int qty[100];
-     double price[100];
-     double tax[100];
-     double amount[100];
+     float Price[100];
+     float tax = 0.18;
+     float amount[100];
 
      for(int i=0;i<totalItem;i++){
-         cin.ignore();
+         cin.ignore(1000,'\n');
          cout << "Name of Item-" << i+1 <<": ";
          getline(cin,names[i]);
+         cout << "Quenty of Item-" << i+1 << ": ";
+         cin >> qty[i];
          cout << "Price of "<< names[i] << " Item: $";
-         cin >> Price;
+         cin >> Price[i];
          cout << endl;
+         
+         amount[i] = Price[i] * qty[i] + (Price[i]*tax);
      }
     print();
     cout << "                        BILL SUMMARY                        " << endl;
@@ -56,18 +59,15 @@ int main(){
     cout << setw(13) << "TAX/UNIT";
     cout << setw(12) << "AMOUNT" << endl;
     print1();
-
-    for(int i=0;i<grandTotal;i++){
-        cout << left
-             << setw(12) << names[i]
-             << setw(11)  << qty[i]
-             << setw(12) << tax[i]
-             << setw(13) << price[i]
-             << setw(12) << amount[i]
-             << endl;
-
-             grandTotal += amount[i];
+    
+    for(int i=0;i<totalItem;i++){
+        cout << left << setw(12) << names[i] << "   ";
+        cout << left << setw(8) << qty[i] << "     ";
+        cout << left << setw(13) << Price[i] << " ";
+        cout << left << setw(13) << tax;
+        cout << left << setw(12) << amount[i] << endl;
     }
+
 
     cout << "\n\n\n\n\n\n\n";
     return 0;
